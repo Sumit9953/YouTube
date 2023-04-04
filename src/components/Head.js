bdbbce07021e72f738dab70d33f6cbeee98bb1e2
sumit <sumitsantosh555@gmail.com>
2023-04-04 13:14:19 +0530

    slidfebar

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,24 +1,39 @@
 import React from "react";
+import { toggleMenu } from "../utils/appSlice";
+import { useDispatch } from "react-redux";
 
 const Head = () => {
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
-    <div className="grid grid-flow-col p-3 m-2 shadow-lg ">
+    <div className="grid grid-flow-col p-3 m-2 shadow-lg align-middle ">
       <div className="flex col-span-1 mx-2">
         <img
-          className="h-10"
+          onClick={() => toggleMenuHandler()}
+          className="h-12 cursor-pointer "
           src="https://static.vecteezy.com/system/resources/previews/002/292/406/original/hamburger-menu-line-icon-free-vector.jpg"
           alt="menu"
         />
-        <img
-          className="h-10"
-          src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"
-          alt="youtube_logo"
-        />
+        <a href="/">
+          <img
+            className="h-12"
+            src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"
+            alt="youtube_logo"
+          />
+        </a>
       </div>
 
       <div className="col-span-10 text-center ">
-        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-400">🔍</button>
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+          type="text"
+        />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-400">
+          🔍
+        </button>
       </div>
 
       <div className="col-span-1">
